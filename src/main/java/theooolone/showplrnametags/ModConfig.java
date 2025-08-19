2a2b40022de1edf6f76ef014617bc785e390749f
Theooolone <110533961+Theooolone@users.noreply.github.com>
2025-08-19 16:16:00 +0100

    Added new settings

--- src/main/java/theooolone/showplrnametags/ModConfig.java
+++ src/main/java/theooolone/showplrnametags/ModConfig.java
@@ -6,4 +6,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = "show-player-nametags")
 public class ModConfig implements ConfigData {
 	public boolean enabled = true;
+	public boolean show_other_player_nametags = true;
+	public boolean show_own_nametag = false;
+	public boolean show_entity_nametags = true;
 }
--- src/main/java/theooolone/showplrnametags/mixin/LivingEntityRendererMixin.java
+++ src/main/java/theooolone/showplrnametags/mixin/LivingEntityRendererMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import theooolone.showplrnametags.ModConfig;
 import theooolone.showplrnametags.ShowPlayerNametags;
 
 @Mixin(LivingEntityRenderer.class)
@@ -16,17 +17,23 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity> {
 	@Inject(method = "hasLabel(Lnet/minecraft/entity/LivingEntity;D)Z", at = @At("TAIL"), cancellable = true)
 	private <T extends LivingEntity> void forceNametagVisibility(T livingEntity, double distance, CallbackInfoReturnable<Boolean> cir) {
 		if (cir.getReturnValue()) return;
-		if (!ShowPlayerNametags.getConfig().enabled) return;
+		ModConfig config = ShowPlayerNametags.getConfig();
+		if (!config.enabled) return;
 
-		MinecraftClient minecraftClient = MinecraftClient.getInstance();
-		ClientPlayerEntity clientPlayerEntity = minecraftClient.player;
+		MinecraftClient client = MinecraftClient.getInstance();
+		ClientPlayerEntity player = client.player;
 
 		boolean hudEnabled = MinecraftClient.isHudEnabled();
 		boolean isPlayer = livingEntity.isPlayer();
-		boolean isCameraEntity = livingEntity == minecraftClient.getCameraEntity();
-		boolean isVisible = !livingEntity.isInvisibleTo(clientPlayerEntity);
+		boolean isCameraEntity = livingEntity == client.getCameraEntity() && !config.show_own_nametag;
+		boolean isVisible = !livingEntity.isInvisibleTo(player);
 		boolean hasPassengers = livingEntity.hasPassengers();
 
-		cir.setReturnValue((hudEnabled || isPlayer) && !isCameraEntity && isVisible && !hasPassengers);
+		cir.setReturnValue(
+				(hudEnabled || (isPlayer ? config.show_other_player_nametags : config.show_entity_nametags))
+						&& (!isCameraEntity || config.show_own_nametag)
+						&& isVisible
+						&& !hasPassengers
+		);
 	}
 }
